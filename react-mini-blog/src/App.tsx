6132938c3f9cf3cd63ff3827f4b774fb3425271c
atete <n.atete@alustudent.com>
2026-09-21 15:20:27 +0200

    feat: add About and Footer components to App

--- react-mini-blog/src/App.tsx
+++ react-mini-blog/src/App.tsx
@@ -1,11 +1,15 @@
 import Header from './components/Header';
+import About from './components/About';
 import PostList from './components/PostList';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <>
       <Header />
+      <About />
       <PostList />
+      <Footer />
     </>
   );
 }
